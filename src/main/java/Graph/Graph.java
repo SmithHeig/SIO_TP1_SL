56e6361535474067fbaa3dc730cf68ae62238517
Unknown <james.smith@heig-vd.ch>
2018-03-15 17:17:36 +0100

    SL Algo started
    to continue

--- src/main/java/Graph/Graph.java
+++ src/main/java/Graph/Graph.java
@@ -10,15 +10,19 @@ public class Graph {
     private static final Logger LOG = Logger.getLogger(Graph.class.getName());
 
     private LinkedList<Integer>[] adjList;
+    private int nbNode;
+    private int nbEdge;
 
     public Graph(File file){
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
             String line = reader.readLine();
             String words[] = line.split(" ");
+            nbNode = Integer.getInteger(words[0]) -1;
+            nbEdge = Integer.getInteger(words[1]) -1;
 
             // initialisation du tableau d'adjacence
-            adjList = new LinkedList[Integer.getInteger(words[0]) - 1];
+            adjList = new LinkedList[nbNode];
 
             // ajout des voisins dans le tableau d'adjacence
             while((line = reader.readLine()) != null){
@@ -32,5 +36,31 @@ public class Graph {
         } catch(IOException e){
             LOG.log(Level.SEVERE,e.getMessage());
         }
+    }
+
+    public int getNbNode(){
+        return nbNode;
+    }
+
+    public int getNbEdge(){
+        return nbEdge;
+    }
+
+    public int getDegreMax(){
+        int degreMax = 0;
+        for(LinkedList<Integer> l : adjList){
+            if(l.size() > degreMax){
+                degreMax = l.size();
+            }
+        }
+        return degreMax;
+    }
+
+    public int getDegreNode(int node){
+        return adjList[node].size();
+    }
+
+    public LinkedList<Integer> getAdjList(int i) {
+        return adjList[i];
     }
 }
